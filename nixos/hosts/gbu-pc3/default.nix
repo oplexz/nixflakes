c4e10dfdd81b1ee5abbc391d7a7f3959f78139d7
Daniil Isakov <12859907+oplexz@users.noreply.github.com>
2024-04-04 16:44:45 +0300

    Add OpenVPN configuration to gbu-pc3

--- nixos/hosts/gbu-pc3/default.nix
+++ nixos/hosts/gbu-pc3/default.nix
@@ -3,6 +3,13 @@
     ./hardware-configuration.nix
   ];
 
+  services.openvpn.servers = {
+    gbubr = {
+      config = ''config /home/oplexz/ovpn/DIsakov.ovpn '';
+      updateResolvConf = true;
+    };
+  };
+
   # powerManagement.powertop.enable = true;
   # services.thermald.enable = true;
 }
